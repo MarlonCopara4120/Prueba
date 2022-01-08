bde429de14fe4de7bcc527d2f110c68fb6754745
Fernandofr7 <lsanchez6604@uta.edu.ec>
2022-01-08 12:23:07 -0500

    PRUEB-3 Se Agregó un Hola Mundo.

--- src/Prueba/Prueba.java
+++ src/Prueba/Prueba.java
@@ -1,9 +1,11 @@
-
 package Prueba;
 
 public class Prueba {
+
     public static void main(String[] args) {
         System.out.println("Hola a Todos");
+        System.out.println("Hola Mundo");
+
     }
- 
+
 }
